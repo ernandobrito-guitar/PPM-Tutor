21589a64d25b5f91354d501e166994cfc23a0979
ernandobrito-guitar <ernandobrito@unicentro.br>
2026-08-14 16:41:41 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -91,7 +91,7 @@ Regras de Atuação:
 
 # Inicializa o modelo Gemini
 model = genai.GenerativeModel(
-    model_name="models/gemini-1.5-flash",
+    model_name="gemini-1.5-pro",
     system_instruction=system_instruction
 )
 
